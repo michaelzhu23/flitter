e297381458b8d71906d2129a992787862cc48aa6
Michael Zhu <mzhu23@gmail.com>
2020-10-07 02:17:52 -0700

    Created PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
 import Post from "./Post";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      postId
+      content
+      postedByUser {
+        userAlias
+        userName
+        profilePictureUrl
+      }
+    }
+  }
+`;
 
 class PostList extends Component {
   render() {
-    const postsToRender = [
-      {
-        id: "1",
-        content: "Testing post 1",
-      },
-      {
-        id: "2",
-        content: "Testing post 2",
-      },
-    ];
-
     return (
-      <div>
-        {postsToRender.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const postsToRender = data.feed;
+          console.log(postsToRender);
+          return (
+            <div>
+              {postsToRender.map((post) => (
+                <Post key={post.postId} post={post} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
