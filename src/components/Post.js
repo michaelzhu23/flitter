68d5f280ff496a13d4ceb19b02912d9cd3d22585
Michael Zhu <mzhu23@gmail.com>
2020-10-08 03:06:52 -0700

    Added edit & delete icons to logged in user's posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 class Post extends Component {
   render() {
+    const canEdit =
+      this.props.post.postedByUser.userId === "1" ? (
+        <div className="post-icons">
+          <i class="fas fa-edit mx-2 text-primary edit-icon"></i>
+          <i class="fas fa-trash-alt mx-2 text-danger delete-icon"></i>
+        </div>
+      ) : null;
+
     return (
       <div className="user-post border p-3">
         <div className="profile-picture mr-2">
@@ -11,9 +19,12 @@ class Post extends Component {
           />
         </div>
         <div className="post-content">
-          <div>
-            <b>{this.props.post.postedByUser.userName}</b> @
-            {this.props.post.postedByUser.userAlias}
+          <div className="post-heading d-flex justify-content-between">
+            <div className="post-user">
+              <b>{this.props.post.postedByUser.userName}</b> @
+              {this.props.post.postedByUser.userAlias}
+            </div>
+            {canEdit}
           </div>
           <div>{this.props.post.content}</div>
           <div className="post-icons mt-3 d-flex justify-content-between">
