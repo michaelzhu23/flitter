968250049a1275e6642881add4f7e71ce3e58f36
Michael Zhu <mzhu23@gmail.com>
2020-10-08 15:14:58 -0700

    Added edit post mutation to edit icon

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
-import { FEED_QUERY, DELETE_POST_MUTATION } from "../Queries";
+import {
+  FEED_QUERY,
+  DELETE_POST_MUTATION,
+  UPDATE_POST_MUTATION,
+} from "../Queries";
 
 class Post extends Component {
+  state = {
+    content: this.props.post.content,
+    editing: false,
+  };
+
+  togglePostEditing = () => {
+    this.setState({ editing: !this.state.editing });
+  };
+
   render() {
+    const { content } = this.state;
+
     const canEdit =
       this.props.post.postedByUser.userId === "1" ? (
         <div className="post-icons">
-          <i className="fas fa-edit mx-2 text-primary edit-icon"></i>
+          <i
+            className="fas fa-edit mx-2 text-primary edit-icon"
+            onClick={this.togglePostEditing}
+          ></i>
           <Mutation
             mutation={DELETE_POST_MUTATION}
             variables={{ postId: this.props.post.postId }}
@@ -22,6 +40,47 @@ class Post extends Component {
           </Mutation>
         </div>
       ) : null;
+
+    const postContent = this.state.editing ? (
+      <>
+        <input
+          className="mb-2"
+          value={content}
+          onChange={(e) => this.setState({ content: e.target.value })}
+          type="text"
+          size="60"
+        />
+        <div className="edit-buttons">
+          <Mutation
+            mutation={UPDATE_POST_MUTATION}
+            variables={{ postId: this.props.post.postId, content }}
+            refetchQueries={[{ query: FEED_QUERY }]}
+          >
+            {(updatePostMutation) => (
+              <button
+                type="button"
+                className="btn btn-outline-primary btn-sm mr-2"
+                onClick={() => {
+                  updatePostMutation();
+                  this.togglePostEditing();
+                }}
+              >
+                Submit
+              </button>
+            )}
+          </Mutation>
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm"
+            onClick={this.togglePostEditing}
+          >
+            Cancel
+          </button>
+        </div>
+      </>
+    ) : (
+      <div className="post-content mt-1">{this.props.post.content}</div>
+    );
 
     return (
       <div className="user-post border p-3">
@@ -39,7 +98,7 @@ class Post extends Component {
             </div>
             {canEdit}
           </div>
-          <div className="post-content mt-1">{this.props.post.content}</div>
+          {postContent}
           <div className="post-icons mt-3 d-flex justify-content-between">
             <i className="far fa-comment"></i>
             <i className="fas fa-redo"></i>
