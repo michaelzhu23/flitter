ba4d77c9348fd14625bdb15ba3f217020f74dcf4
Michael Zhu <mzhu23@gmail.com>
2020-10-09 04:15:57 -0700

    Created Header component and added to App's render

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import PostList from "./PostList";
 import CreatePost from "./CreatePost";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <>
+        <Header />
         <CreatePost />
+        <hr className="divider"/>
         <PostList />
       </>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <section className="col-12 border-bottom">
+        <div className="row heading col-12 p-0 d-flex justify-content-between align-items-center">
+          <h3 className="m-0 ml-5">
+            <i className="fab fa-earlybirds text-info h1 mr-2"></i>
+            <b>Flitter</b>
+          </h3>
+          <div className="header-icons d-flex justify-content-between align-items-end">
+            <div className="header-picture d-flex align-items-end">
+              <img
+                src="./images/michael-zhu.jpg"
+                alt="Logged in User"
+                className="mr-1"
+              />
+              <h5><b>Michael</b></h5>
+            </div>
+            <i class="fas fa-home h3 text-info"></i>
+            <i class="fas fa-envelope h3 text-info"></i>
+            <i class="fas fa-bookmark h3 text-info"></i>
+            <i class="fas fa-bell h3 text-info"></i>
+            <i class="fas fa-user h3 text-info"></i>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
